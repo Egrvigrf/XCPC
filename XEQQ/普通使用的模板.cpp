9a9895015265f366f29dc6e64f4be40a5c955317
2479452897@qq.com <>
2024-08-14 16:56:08 +0800

    qqqqq

---
+++ XEQQ/普通使用的模板.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+typedef pair<int, int> pi;
+const int N = 2e5 + 5;
+int a[N];
+int n, m, k, w;
+void solve()
+{
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
